b9988c5377ec4fd75539de50e7b8bab6cc792d12
abirBluetech <abir.sakif@gmail.com>
2019-01-23 10:55:17 +0600

    practice question of chapter 2 solved

---
+++ chapter02/practiceQues13Solution.py
@@ -0,0 +1,14 @@
+# Write a short program that
+# prints the numbers 1 to 10 using a for loop.
+# Then write an equivalent program that
+# prints the numbers 1 to 10 using a while loop.
+
+print('Printing the numbers 1 to 10 using for loop')
+for i in range(1, 11):
+    print(i)
+
+print('Printing the numbers 1 to 10 using while loop')
+i = 1
+while i < 11:
+    print(i)
+    i = i + 1
